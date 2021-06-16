ead0d8b70d235b0eb94c011ecb4cce71b0728595
mhendricks96 <mhendricks96@yahoo.com>
2021-06-16 10:38:30 -0700

    set up musicioan classes and got first 13 tests passing

--- pythonic_garage_band/band.py
+++ pythonic_garage_band/band.py
@@ -9,9 +9,10 @@ class Band:
     def __repr__(self):
         return f"Band instance. name={self.name}, members={self.members}"
 
-# "Guitarist instance. Name = Joan Jett"
+# "Drummer instance. Name = Sheila E."
 class Musician:
     pass
+
 
 class Guitarist(Musician):
     def __init__(self, name):
@@ -22,9 +23,41 @@ class Guitarist(Musician):
 
     def __str__(self):
         return f"My name is {self.name} and I play guitar"
+    
+    def get_instrument(self):
+        return "guitar"
+
+    def play_solo(self):
+        return ""
 
 class Bassist(Musician):
-    pass
+    def __init__(self, name):
+        self.name = name
+
+    def __repr__(self):
+        return f"Bassist instance. Name = {self.name}"
+
+    def __str__(self):
+        return f"My name is {self.name} and I play bass"
+
+    def get_instrument(self):
+        return "bass"
+
+    def play_solo(self):
+        return ""
 
 class Drummer(Musician):
-    pass
+    def __init__(self, name):
+        self.name = name
+    
+    def __repr__(self):
+        return f"Drummer instance. Name = {self.name}"
+
+    def __str__(self):
+        return f"My name is {self.name} and I play drums"
+
+    def get_instrument(self):
+        return "drums"
+
+    def play_solo(self):
+        return ""
